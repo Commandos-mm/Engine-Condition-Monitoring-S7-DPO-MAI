fa43635b6a5e23cee16218711b4732e7fcd48324
Ivan Nedosekov <ivan-nedd@mail.ru>
2023-05-13 00:51:03 +0300

    worked ui

--- app/src/main.py
+++ app/src/main.py
@@ -1,39 +1,46 @@
 from typing import TypeAlias
 
-# import streamlit as st
+import altair as alt
+from collections import defaultdict
+
+import streamlit as st
 import pandas as pd
 
 import pipeline
 
-
-# uploaded_file = st.file_uploader("Choose a file")
-# if uploaded_file is not None:
-#     ts = pd.read_csv(uploaded_file.getvalue())
-# else:
-#     idx = pd.date_range("2018-01-01", periods=5, freq="H")
-#     ts = pd.Series(range(len(idx)), index=idx)
+from pipeline.predictor import EngineFamily, FlightPhase
 
 
-# st.line_chart(ts)
+uploaded_file = st.file_uploader("Choose a file")
+if uploaded_file is not None:
+    ts = pd.read_csv(uploaded_file.getvalue())
+else:
+    idx = pd.date_range("2018-01-01", periods=5, freq="H")
+    ts = pd.Series(range(len(idx)), index=idx)
 
-ts = ''
+
+st.line_chart(ts)
+
+ts = pd.read_csv('src/pipeline/data/X.csv')
 engine_inference_mapping = pipeline.predict(ts)
 
 
-for family_id, inference in engine_inference_mapping.items():
-    for metric_name in set(inference.columns).difference(['timestamp', 'n1_modifier']):
-        metric_df = inference[['timestamp', 'flight_phase', metric_name]]
+
+
+for (phase, family_id), inference in engine_inference_mapping.items():
+    
+    metric_column_names = {'ZT49_D'}
+
         
-        drawing_dataset = pd.DataFrame(
-            {
-                'timestamp': metric_df['timestamp'],
-                'cruise': metric_df[metric_df['flight_phase'] == 'CRUISE'],
-                'takeoff': metric_df[metric_df['flight_phase'] =='TAKEOFF'],
-            }
-        )
-        print(drawing_dataset)
-        exit()
-        st.line_chart(drawing_dataset)
+        
+    drawing_dataset = inference[['flight_datetime', *metric_column_names]].astype({'flight_datetime': 'datetime64[ns]'})
+    print(drawing_dataset.dtypes)
+    chart = alt.Chart(drawing_dataset).mark_circle().encode(
+    x='flight_datetime:T',
+    y='ZT49_D:Q',
+    # color='Origin',
+).interactive()
+    st.altair_chart(chart, theme="streamlit", use_container_width=True)
 
 
 
--- app/src/pipeline/predictor.py
+++ app/src/pipeline/predictor.py
@@ -1,10 +1,12 @@
-from typing import TypeAlias
+from typing import TypeAlias, Literal
 
 import pandas as pd
-from typing import Dict, Tuple, Any
 
+FlightPhase = Literal['CRUISE', 'TAKEOFF']
+EngineFamily: TypeAlias = str
+ 
 
-def predict(input: pd.DataFrame) -> Dict[Tuple[Any], pd.DataFrame]:
+def predict(input: pd.DataFrame) -> dict[tuple[FlightPhase, EngineFamily], pd.DataFrame]:
     """
     Mock function for maintenance characteristics prediction.
     :param input: input DataFrame of aircraft and engine characteristics.
@@ -13,8 +15,8 @@ def predict(input: pd.DataFrame) -> Dict[Tuple[Any], pd.DataFrame]:
     output: pd.DataFrame = pd.read_csv("src/pipeline/data/y.csv")
     sub_input: pd.DataFrame = input[["engine_id", "flight_datetime", "flight_phase", "engine_family"]]
     df: pd.DataFrame = pd.merge(sub_input, output, on=["engine_id", "flight_datetime", "flight_phase"])
-    phase_df: Dict = {k: v for k, v in df.groupby("flight_phase")}
-    grouped_output: Dict = {
+    phase_df = {k: v for k, v in df.groupby("flight_phase")}
+    grouped_output = {
         (phase_key, engine_family_key): df_value
         for phase_key, df in phase_df.items()
         for engine_family_key, df_value in df.groupby("engine_family")
