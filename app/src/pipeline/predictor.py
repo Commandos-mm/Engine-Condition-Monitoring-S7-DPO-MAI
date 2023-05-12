72fe27e806c5f954a7b8c931cf14dd7e082f4e8e
san-kir-k <dfomka@list.ru>
2023-05-12 21:41:18 +0300

    Make group stage of predict function more general

--- app/src/pipeline/predictor.py
+++ app/src/pipeline/predictor.py
@@ -1,20 +1,39 @@
 import pandas as pd
-from typing import Dict, Tuple, Any
+from typing import Dict, Tuple, Set, Callable
 
 
-def predict(input: pd.DataFrame) -> Dict[Tuple[Any], pd.DataFrame]:
+def grouper(input: Dict[Tuple[str, ...],pd.DataFrame],
+            keys: Tuple[str, ...]) -> Dict[Tuple[str, ...], pd.DataFrame]:
+    """
+    Helper function for recursive grouping dict of pandas DataFrames.
+    """
+    if not keys:
+        return input
+
+    to_tuple_transformer: Callable = lambda element: element if isinstance(element, tuple) else (element, )
+
+    grouped_df: Dict[Tuple[str, ...], pd.DataFrame] = {
+        (next_key, *to_tuple_transformer(curr_key)): next_df
+        for curr_key, curr_df in input.items()
+        for next_key, next_df in curr_df.groupby(keys[-1])
+    }
+
+    return grouper(grouped_df, keys[:-1])
+
+
+def predict(input: pd.DataFrame,
+            keys: Tuple[str, ...] = ("engine_family", "flight_phase")) -> Dict[Tuple[str, ...], pd.DataFrame]:
     """
     Mock function for maintenance characteristics prediction.
     :param input: input DataFrame of aircraft and engine characteristics.
+    :param keys: keys that are used for grouping output data.
     :return: output groups of DataFrames of predicted maintenance characteristics.
     """
+    # TODO: add model prediction instead of valid data
     output: pd.DataFrame = pd.read_csv("src/pipeline/data/y.csv")
-    sub_input: pd.DataFrame = input[["engine_id", "flight_datetime", "flight_phase", "engine_family"]]
+    sub_input_keys: Set[str, ...] = {"engine_id", "flight_datetime", "flight_phase", *keys}
+    sub_input: pd.DataFrame = input[list(sub_input_keys)]
     df: pd.DataFrame = pd.merge(sub_input, output, on=["engine_id", "flight_datetime", "flight_phase"])
-    phase_df: Dict = {k: v for k, v in df.groupby("flight_phase")}
-    grouped_output: Dict = {
-        (phase_key, engine_family_key): df_value
-        for phase_key, df in phase_df.items()
-        for engine_family_key, df_value in df.groupby("engine_family")
-    }
-    return grouped_output
+    phase_df: Dict = {k: v for k, v in df.groupby(keys[-1])}
+
+    return grouper(phase_df, keys[:-1])
