b46c94efe95765f9e73f1f844922e8094f1b2bda
Ivan Nedosekov <ivan-nedd@mail.ru>
2023-05-12 22:18:44 +0300

    tmp

--- app/src/main.py
+++ app/src/main.py
@@ -1,2 +1,38 @@
-if __name__ == '__main__':
-    print('Hello MM!')
+from typing import TypeAlias
+
+import streamlit as st
+import pandas as pd
+
+
+
+uploaded_file = st.file_uploader("Choose a file")
+if uploaded_file is not None:
+    ts = pd.read_csv(uploaded_file.getvalue())
+else:
+    idx = pd.date_range("2018-01-01", periods=5, freq="H")
+    ts = pd.Series(range(len(idx)), index=idx)
+
+
+st.line_chart(ts)
+
+EngineFamilyId: TypeAlias = str
+
+
+engine_inference_mapping: dict[EngineFamilyId, pd.DataFrame]  = pipeline(ts)
+
+
+for family_id, inference in engine_inference_mapping.items():
+    for metric_name in set(inference.columns).difference(['timestamp', 'n1_modifier']):
+        metric_df = inference[['timestamp', 'flight_phase', metric_name]]
+        
+        drawing_dataset = pd.DataFrame(
+            {
+                'timestamp': metric_df['timestamp'],
+                'cruise': metric_df[metric_df['flight_phase'] == 'CRUISE'],
+                'takeoff': metric_df[metric_df['flight_phase'] =='TAKEOFF'],
+            }
+        )
+        st.line_chart(drawing_dataset)
+
+
+
