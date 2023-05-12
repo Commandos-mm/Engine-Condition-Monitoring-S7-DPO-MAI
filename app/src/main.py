813021dd9ce3f7699af2f2dc431b1cdcb7780011
Ivan Nedosekov <ivan-nedd@mail.ru>
2023-05-13 01:16:02 +0300

    start components

--- app/src/main.py
+++ app/src/main.py
@@ -9,37 +9,24 @@ import pandas as pd
 import pipeline
 
 from pipeline.predictor import EngineFamily, FlightPhase
+from ui_components.chart import get_chart
 
 
 uploaded_file = st.file_uploader("Choose a file")
 if uploaded_file is not None:
     ts = pd.read_csv(uploaded_file.getvalue())
 else:
-    idx = pd.date_range("2018-01-01", periods=5, freq="H")
-    ts = pd.Series(range(len(idx)), index=idx)
 
+    ts = pd.read_csv('src/pipeline/data/X.csv')
 
-st.line_chart(ts)
-
-ts = pd.read_csv('src/pipeline/data/X.csv')
 engine_inference_mapping = pipeline.predict(ts)
 
 
 
 
 for (phase, family_id), inference in engine_inference_mapping.items():
-    
-    metric_column_names = {'ZT49_D'}
+    chart = get_chart(inference)
 
-        
-        
-    drawing_dataset = inference[['flight_datetime', *metric_column_names]].astype({'flight_datetime': 'datetime64[ns]'})
-    print(drawing_dataset.dtypes)
-    chart = alt.Chart(drawing_dataset).mark_circle().encode(
-    x='flight_datetime:T',
-    y='ZT49_D:Q',
-    # color='Origin',
-).interactive()
     st.altair_chart(chart, theme="streamlit", use_container_width=True)
 
 
