1fcdbb3b16774833e591fd9d9c5901617fbfb2af
Ivan Nedosekov <ivan-nedd@mail.ru>
2023-05-13 20:13:26 +0300

    refactor

--- app/src/main.py
+++ app/src/main.py
@@ -12,12 +12,6 @@ ts = components.load_dataset()
 
 engine_inference_mapping = pipeline.predict(ts)
 
-# TODO: remove
-HARDCODED_ENGINE_FAMILY: str = "CFM56-7"
-HARDCODED_ENGINE_ID: str = "364a94e57c5e7705c650bdadda955186d3a8f9d96a3d9ec0f32964ef9c09b494"
+for family_id, inference_by_phase in engine_inference_mapping.items():
+    components.chart.family_accordion(family_id, inference_by_phase)
 
-single_inference = engine_inference_mapping[HARDCODED_ENGINE_FAMILY][HARDCODED_ENGINE_ID]
-chart_takeoff = components.chart.get_chart(single_inference['TAKEOFF'])
-chart_cruise = components.chart.get_chart(single_inference['CRUISE'])
-with components.PageLayout() as page:
-    page.altair_chart(chart_takeoff | chart_cruise, theme="streamlit", use_container_width=True)
