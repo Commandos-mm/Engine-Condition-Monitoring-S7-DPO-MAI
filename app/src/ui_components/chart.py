0888653ad3efd21503a8f46a25c8355db3a26ebe
Ivan Nedosekov <ivan-nedd@mail.ru>
2023-05-14 12:49:11 +0300

    slider

--- app/src/ui_components/chart.py
+++ app/src/ui_components/chart.py
@@ -6,18 +6,46 @@ from .page_layout_manager import PageLayout
 
 metric_column_names = {'ZT49_D'}
 
+def get_dates_range(engine_df: dict[str, pd.DataFrame]):
+    print(engine_df['TAKEOFF'].dtypes)
+    engine_df['TAKEOFF'].flight_datetime = engine_df['TAKEOFF'].flight_datetime.astype('datetime64[ns]')
+    engine_df['CRUISE'].flight_datetime  = engine_df['CRUISE'].flight_datetime .astype('datetime64[ns]')
+    min_takeoff_ts, max_takeoff_ts = engine_df['TAKEOFF']['flight_datetime'].agg(['min', 'max'])
+    min_cruise_ts, max_cruise_ts = engine_df['CRUISE']['flight_datetime'].agg(['min', 'max'])
+    min_ts = min(min_cruise_ts, min_takeoff_ts)
+    max_ts = max(max_cruise_ts, max_takeoff_ts)
+
+    return min_ts.to_pydatetime(), max_ts.to_pydatetime()
+
+
+
+
+def engine_flight_date_slider(engine_df: pd.DataFrame):
+    date_range = get_dates_range(engine_df)
+    
+    return st.slider('Date range', value=date_range)
+
+
 def family_page_info(engine_family_id, family_inference):
     engine_id = st.selectbox(
         'Engine ID',
         tuple(family_inference.keys()),
         key = engine_family_id
     )
-    engine_graphics(family_inference[engine_id])
+    engine_df = family_inference[engine_id]
+    date_range = engine_flight_date_slider(engine_df)
+    engine_graphics(family_inference[engine_id], date_range)
 
+def slice_df(dataset: pd.DataFrame, ts_range):
+    df = dataset
+    min_ts, max_ts = ts_range
+    filtered_df = df[(df['flight_datetime'] >= min_ts) & (df['flight_datetime'] <= max_ts)]
+    return filtered_df
+      
 
-def engine_graphics(engine_inference):
-    chartl = get_chart(engine_inference['TAKEOFF'])
-    chartr = get_chart(engine_inference['CRUISE'])
+def engine_graphics(engine_inference, date_range):
+    chartl = get_chart(slice_df(engine_inference['TAKEOFF'], date_range))
+    chartr = get_chart(slice_df(engine_inference['CRUISE'], date_range))
     with PageLayout() as page:
         page.altair_chart(chartl | chartr, theme="streamlit", use_container_width=True)
 
