b67f1dd5244a50b9a772e864c26831ce6ddcb65e
Ivan Nedosekov <ivan-nedd@mail.ru>
2023-05-16 00:12:19 +0300

    add metrics

--- app/src/ui_components/chart.py
+++ app/src/ui_components/chart.py
@@ -63,7 +63,7 @@ def family_page_info(engine_family_id, family_inference):
     )
     engine_df = family_inference[engine_id]
     date_range = engine_flight_date_slider(engine_df)
-    engine_graphics(family_inference[engine_id], date_range)
+    engine_graphics(family_inference[engine_id], date_range, engine_id)
 
 
 def slice_df(datasets: dict[str, pd.DataFrame], ts_range):
@@ -76,7 +76,7 @@ def slice_df(datasets: dict[str, pd.DataFrame], ts_range):
 }
 
 
-def metric_graphics(metric_name: str, engine_inference, date_range):
+def metric_graphics(metric_name: str, engine_inference, date_range, e_id):
     with st.expander(metric_name):
         chartl = get_chart(slice_df(engine_inference['TAKEOFF'], date_range), metric_name)
         chartr = get_chart(slice_df(engine_inference['CRUISE'], date_range), metric_name)
@@ -88,13 +88,55 @@ def metric_graphics(metric_name: str, engine_inference, date_range):
 
             if desc := METRIC_DECRIPTION.get(metric_name):
                 page.markdown(f"{metric_name} — {desc}")
+            metric_table(engine_inference['TAKEOFF'], metric_name, e_id)
 
 
-def engine_graphics(engine_inference, date_range):
+def calculate_error(real_y: pd.DataFrame, predicted_y, metric_name):
+    merged_df = real_y.merge(predicted_y, on=['flight_datetime'])
+    merged_df = merged_df.set_index('flight_datetime')
+    return (
+        (merged_df[f'{metric_name}_x'] - merged_df[f'{metric_name}_y']).abs()
+    )
+
+def slice_metrics(df: pd.Series, from_, to_):
+    return df[ (df >= from_) & (df <= to_) ]
+
+
+def metric_table(inference, metric_name, e_id):
+
+    with st.expander('Abs Error Table'):
+        if st.button('Calculate abs error', key=f'{metric_name}_{e_id}'):
+            with st.spinner('Calculating...'):
+                error = calculate_error(inference['real_y'], inference['predicted_y'], metric_name)
+            
+            if not error.empty and error.isnull().values.any():
+                print('Escape')
+                return
+            min_v, max_v = error.agg(['min', 'max'])
+            lborder, rborder = st.slider('Error value', value=(min_v, max_v))
+
+            error = slice_metrics(error, lborder, rborder).to_frame().style.applymap(color_metric)
+            
+            st.table(error)
+            
+        
+
+
+def color_metric(val):
+    red, green = (255, 0, 0), (0, 255, 0)
+    val_norm = abs(val) / 3.0
+    ratio = min(val_norm, 1.0)
+    r = int(red[0] * ratio + green[0] * (1 - ratio))
+    g = int(red[1] * ratio + green[1] * (1 - ratio))
+    b = int(red[2] * ratio + green[2] * (1 - ratio))
+    return f"background-color: rgba({r},{g},{b}, 0.4)"
+
+
+def engine_graphics(engine_inference, date_range, e_id):
     metric_names = set(engine_inference['TAKEOFF']['predicted_y'].columns).difference(['flight_datetime'])
 
     for metric_name in metric_names:
-        metric_graphics(metric_name, engine_inference, date_range)
+        metric_graphics(metric_name, engine_inference, date_range, e_id)
 
 
 def family_accordion(engine_family_id: str, family_inference: dict):
@@ -108,17 +150,6 @@ def get_chart(datasets: dict[str, pd.DataFrame], metric_name: str) -> alt.Chart:
         predicted_y[['flight_datetime', metric_name]]
     )
     color_options = {}
-    # real_y = datasets.get('real_y')
-    # if real_y is not None:
-    #     real_y = (
-    #         real_y[['flight_datetime', metric_name]]
-    #     )
-
-    #     predicted_y['label'] = 'predicted_y'
-    #     real_y['label'] = 'real_y'
-    #     color_options['color'] = 'label:N'
-    #     drawing_dataset = pd.concat((predicted_y, real_y))
-    # else:
     drawing_dataset = predicted_y
     
 
